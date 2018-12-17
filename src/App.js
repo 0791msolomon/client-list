d02d56c9b8c195d50f7a5b53d7bb3351087068db
Matt <matthew@realtimereviews.com>
2018-12-17 09:19:10 -0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { auth, db } from "./Auth";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    businessName: "",
+    bID: "",
+    tier: "",
+    dateCreated: "",
+    companyData: ""
+  };
+
+  componentDidMount = () => {
+    let companyData = [];
+    auth.onAuthStateChanged(user => {
+      db.ref(`companies`)
+        .once("value", snap => {
+          let x = snap.val();
+          let entries = Object.entries(x);
+          companyData.push(entries);
+        })
+        .then(() => this.setState({ companyData }));
+    });
+  };
+
+  renderCompanyData = () => {
+    let { companyData } = this.state;
+    return companyData[0].map(item => {
+      return (
+        <tr key={item[0]}>
+          <td className="col-sm-4">{item[1].name}</td>
+          <td className="col-sm-4">{item[0]}</td>
+          <td className="col-sm-2">{item[1].tier}</td>
+          <td className="col-sm-2">{this.state.date}</td>
+        </tr>
+      );
+    });
+  };
+
   render() {
+    if (!this.state.companyData) {
+      return <h1>Loading...</h1>;
+    }
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+      <div>
+        <nav className="navbar navbar-default">
+          <div className="navbar-header navbar-left">
+            <a className="navbar-brand">Client List</a>
+          </div>
+          <form
+            className="navbar-form navbar-right navbar-search"
+            role="search"
           >
-            Learn React
-          </a>
-        </header>
+            <div className="form-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search"
+              />
+            </div>
+            <button type="submit" className="btn btn-info ">
+              Submit
+            </button>
+          </form>
+        </nav>
+        <table className="table table-bordered table-dark">
+          <thead>
+            <tr>
+              <th scope="col">Company Name</th>
+              <th scope="col">Company ID</th>
+              <th scope="col">Tier</th>
+              <th scope="col">Created Date</th>
+            </tr>
+          </thead>
+          <tbody>{this.renderCompanyData()}</tbody>
+        </table>
       </div>
     );
   }
